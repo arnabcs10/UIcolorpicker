6b6cdea09c1811feba9ab2dd06f0c3e5f253e744
arnab <arnab10panda@gmail.com>
2020-10-05 22:45:56 +0530

    Add CopyToClipBoard and CopyOverlay Animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
  class ColorBox extends Component {
+    constructor(props){
+        super();
+        this.state={
+            copied: false
+        };
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
     
+    changeCopyState(){
+        this.setState({ copied:true } , ()=>{
+            setTimeout(() => 
+                this.setState({
+                    copied:false
+                })
+            , 1500);
+        });
+        
+    }
     render() {
         const {name , background } = this.props;
         return (
-            <div className='ColorBox' style={{backgroundColor:background}}>
-                <div className="copy-container">
-                    <div className="box-content">
-                    <span>{name}</span>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div className='ColorBox' style={{backgroundColor:background}} >
+                    <div className={`copy-overlay ${this.state.copied ? 'show':''}`} style={{backgroundColor:background}} />
+                    <div className={`copy-msg ${this.state.copied ? 'show':''}`}>
+                        <h1>COPIED!</h1>
+                        <p>{ this.props.background }</p>
                     </div>
-                    <button className='copy-button'>copy</button>
+                    <div className="copy-container">
+                        <div className="box-content">
+                        <span>{name}</span>
+                        </div>
+                        <button className='copy-button'>copy</button>
+                    </div>
+                    <span className='see-more'>More</span>
                 </div>
-                <span className='see-more'>More</span>
-            </div>
+            </CopyToClipboard>
         );
     }
 }
