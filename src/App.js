f68cf8e2e8705dcf470ed1bed6d48fac60d787f6
arnab <arnab10panda@gmail.com>
2020-10-06 21:44:23 +0530

    Add colorHelpers.js to generate shades of colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { genaratePalette } from './colorHelpers';
 function App() {
+  console.log(genaratePalette(seedColors[1]));
   return (
+    
     <div className="App">
       <Palette {...seedColors[2]}/>
     </div>
