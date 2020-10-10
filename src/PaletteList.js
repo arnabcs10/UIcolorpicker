e26f108ac5c2edfb1dee35037832ade8781d0b1e
arnab <arnab10panda@gmail.com>
2020-10-10 12:57:00 +0530

    Start styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import {withStyles} from '@material-ui/styles';
 import {Link} from 'react-router-dom';
 import MiniPalette from './MiniPalette';
+
+const styles = {
+    root:{
+        backgroundColor:"blue",
+        height:"100%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"flex-start"
+    },
+    container:{
+        width:"50%",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"flex-start",
+        flexWrap:"wrap",
+
+    },
+    nav:{
+        width:"100%",
+        display:"flex",
+        justifyContent:"space-between",
+        color:"white"
+    },
+    palettes:{
+       boxSizing:"border-box",
+       width:"100%",
+       display:"grid",
+       gridTemplateColumns:"repeat(3,30%)",
+       gridGap:"5%"
+    }
+}
+
 class PaletteList extends Component {
     render() {
-        const { palettes } = this.props;
+        const { palettes , classes} = this.props;
         return (
-            <div>
-                <h1>React colors</h1>
-                {palettes.map( palette => (
-                    <MiniPalette {...palette}/>
-                ) )}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <div className={classes.nav}>
+                        <h1>React colors</h1>
+                    </div>
+                    <div className={classes.palettes}>
+                        {palettes.map( palette => (
+                            <MiniPalette {...palette}/>
+                        ) )}
+                    </div>
+                </div>   
             </div>
         );
     }
 }
 
-export default PaletteList;
-{/* <p>
-                    <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p> */}
+export default  withStyles(styles)(PaletteList);
