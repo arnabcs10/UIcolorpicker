20f86c4748ddcabbf6d5373a977fdd0ba7af456c
arnab <arnab10panda@gmail.com>
2020-10-12 13:27:59 +0530

    Adjut text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {Link} from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
  class ColorBox extends Component {
     constructor(props){
-        super();
+        super(props);
         this.state={
-            copied: false
+            copied: false,
+            // textColor : this.setTextColor(this.props.background)
         };
         this.changeCopyState = this.changeCopyState.bind(this);
     }
-    
+    // setTextColor(color)
+    // {
+    //     let textColor;
+    //     if( chroma(color).luminance() > 0.5 )
+    //         textColor="black";
+    //     else
+    //         textColor="white";
+    //     console.log(chroma(color).luminance());   
+    //     console.log(textColor); 
+    //     return    textColor; 
+    // }
     changeCopyState(){
         this.setState({ copied:true } , ()=>{
             setTimeout(() => 
@@ -23,23 +35,25 @@ import './ColorBox.css';
     }
     render() {
         const {name , background,colorId,paletteId,showMore } = this.props;
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() >= 0.5;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className='ColorBox' style={{backgroundColor:background}} >
                     <div className={`copy-overlay ${this.state.copied ? 'show':''}`} style={{backgroundColor:background}} />
-                    <div className={`copy-msg ${this.state.copied ? 'show':''}`}>
+                    <div className={`copy-msg ${this.state.copied ? 'show':''} ${isLightColor && "dark-text"}`}>
                         <h1>COPIED!</h1>
                         <p>{ this.props.background }</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                        <span>{name}</span>
+                        <span className={isDarkColor && ("light-text")}>{name}</span>
                         </div>
                         <button className='copy-button'>copy</button>
                     </div>
                     { showMore && (
                         <Link to={`/palette/${paletteId}/${colorId}`} onClick={ (e)=> e.stopPropagation()}>
-                            <span className='see-more'>More</span>
+                            <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
                         </Link>
                     )}
                     
